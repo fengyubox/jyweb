4594be14d548a008f1d099c5fab63c671c744ac4
fengyubox <30491272+fengyubox@users.noreply.github.com>
2019-11-29 00:09:50 +0800

    test
    first

---
+++ Scripts/core.js
@@ -0,0 +1,31 @@
+﻿var vm = new Vue({
+  el:"#mydiv",
+  data:{
+    // 默认的用户名
+    uname:"",
+    // 默认的用户密码
+    upass:"",
+    // 是否显示忘记密码
+    forgetPassShowroNot:true,
+    // 默认的用户名默认值
+    accountPlaceholder:"手机号/邮箱/会员名",
+    // 是否是用户账户登录，默认是 true
+    isUserAccount:true,
+    // 默认短信验证码的值
+    getCode:'',
+    // 切换登录方式按钮默认显示的文本
+    defaultAccountText:'短信验证码登录' ,
+  },
+  methods:{
+    // 登录显示模态框 调用 mint-ui 的 messagebox
+    login() {
+       window.location.href="pay.ashx"; 
+
+    },
+    // 切换登录方式
+    changeLoginMethod() {
+
+       window.location.href="findpay.ashx"; 
+    }
+  }
+})
